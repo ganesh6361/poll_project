564a6e4787641c63dab61b7ea86422bd8479683c
ganesh <bandarug83@gmail.com>
2020-12-01 11:04:32 +0530

    make it better

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import psycopg2
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
